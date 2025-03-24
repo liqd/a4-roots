f9c1dbbb4c1af2896c27896c2c30f512f939d3f2
Mara Karagianni <m.karagianni@liqd.net>
2025-03-24 19:56:22 +0100

    apps: learning nuggets fix label, tests, templates

--- apps/learning_nuggets/urls.py
+++ apps/learning_nuggets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import re_path
-from .views import LearningCenterView, LearningCategoryView, LearningNuggetView
 
-app_name = 'learning_nuggets'
+from .views import LearningCategoryView
+from .views import LearningCenterView
+from .views import LearningNuggetView
+
+app_name = "learning_nuggets"
 
 urlpatterns = [
     re_path(
@@ -17,6 +20,6 @@ urlpatterns = [
     re_path(
         r"^(?P<category_slug>[-\w_]+)/(?P<nugget_slug>[-\w_]+)/$",
         LearningNuggetView.as_view(),
-        name="nugget",
+        name="nugget-detail",
     ),
 ]
