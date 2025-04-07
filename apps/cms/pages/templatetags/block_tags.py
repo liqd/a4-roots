6df7eb13be6d796cad1454bd6a8b9d2f52514323
Annabel Church <215145+arc64@users.noreply.github.com>
2025-04-07 10:19:44 +0200

    Add additional filetypes

--- apps/cms/pages/templatetags/block_tags.py
+++ apps/cms/pages/templatetags/block_tags.py
@@ -5,13 +5,22 @@ register = template.Library()
 
 @register.simple_tag
 def file_type(media_file):
+    media_file = media_file.lower()
     if media_file.endswith(".mp4"):
         return "video/mp4"
     elif media_file.endswith(".webm"):
         return "video/webm"
+    elif media_file.endswith(".mov"):
+        return "video/quicktime"
+    elif media_file.endswith(".avi"):
+        return "video/x-msvideo"
+    elif media_file.endswith(".mkv"):
+        return "video/x-matroska"
     elif media_file.endswith(".mp3"):
-        return "audio/mp3"
+        return "audio/mpeg"
     elif media_file.endswith(".wav"):
         return "audio/wav"
+    elif media_file.endswith(".ogg"):
+        return "audio/ogg"
     else:
         return "type invalid"
