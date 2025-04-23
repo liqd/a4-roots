0bde1c69c4d6c321b2d65c76c885c0e31b25f1e8
Josh <195121232+partizipation@users.noreply.github.com>
2025-04-23 16:45:43 +0200

    learning nuggets: add related resources only if exists #20

--- apps/learning_nuggets/templatetags/learning_nuggets_tags.py
+++ apps/learning_nuggets/templatetags/learning_nuggets_tags.py
@@ -11,3 +11,9 @@ def get_permission(permission_level):
         "moderator": "a4_candy_learning_nuggets.view_moderator_content",
     }
     return permissions.get(permission_level, "")
+
+
+@register.filter(name="filter_by_block_type")
+def filter_by_block_type(blocks, block_type):
+    """Filters StreamField blocks by their block_type."""
+    return [block for block in blocks if block.block_type == block_type and block.value]
