8e941e2110ab769863c7bcb6a4cd42032e548830
partizipation <195121232+partizipation@users.noreply.github.com>
2025-05-06 15:31:17 +0200

    theming: fix avatar fallback random select (#28)

--- apps/users/models.py
+++ apps/users/models.py
@@ -171,12 +171,12 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
 
     @cached_property
     def avatar_fallback(self):
-        number = self.pk % 5
+        number = self.pk % 4
         return static("images/avatar-{0:02d}.svg".format(number))
 
     @cached_property
     def avatar_fallback_png(self):
-        number = self.pk % 5
+        number = self.pk % 4
         return static("images/avatar-{0:02d}.png".format(number))
 
     def get_short_name(self):
